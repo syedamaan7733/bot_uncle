03edcd57d5f55b3f43e320cabc1785700041478e
iftekhar1405 <iftekharahmedxyz@gmail.com>
2026-04-12 17:21:38 +0530

    fix

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
+import type { StringValue } from 'ms';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
@@ -14,7 +15,8 @@ import { JwtStrategy } from './strategies/jwt.strategy';
             useFactory: (configService: ConfigService) => ({
                 secret: configService.get<string>('JWT_SECRET'),
                 signOptions: {
-                    expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '7d',
+                    expiresIn: (configService.get<string>('JWT_EXPIRES_IN') ||
+                        '7d') as StringValue,
                 },
             }),
         }),
